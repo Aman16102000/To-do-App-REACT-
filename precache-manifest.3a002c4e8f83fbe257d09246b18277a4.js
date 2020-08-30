5bf9863bd60294f3c68722b6ec267e94fd0075bb
Aman16102000 <amanthakur5908@gmail.com>
2020-08-30 17:50:09 +0530

    Updates

---
+++ precache-manifest.3a002c4e8f83fbe257d09246b18277a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d659cf7af7ff30767d8db86951fc172",
+    "url": "/To-do-App-REACT-/index.html"
+  },
+  {
+    "revision": "867d1acf322d742c8684",
+    "url": "/To-do-App-REACT-/static/css/main.79c8a0ef.chunk.css"
+  },
+  {
+    "revision": "d09401dd82f4d495427a",
+    "url": "/To-do-App-REACT-/static/js/2.ad5aac3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-App-REACT-/static/js/2.ad5aac3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867d1acf322d742c8684",
+    "url": "/To-do-App-REACT-/static/js/main.0954d0cc.chunk.js"
+  },
+  {
+    "revision": "f418e293de688883e5c9",
+    "url": "/To-do-App-REACT-/static/js/runtime-main.9f8ce138.js"
+  }
+]);
